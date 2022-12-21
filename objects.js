b9944d60f66bff41a3c728e1c6ee9d662fdd7c3c
HarishKorapati <korapatiharish2@gmail.com>
2022-12-21 15:59:47 +0530

    creating objects by harish

---
+++ objects.js
@@ -0,0 +1,26 @@
+// objects  1, object literal  2, new key word
+ 
+
+let hemanth={}  // creating object
+
+hemanth.firstname="Hemanth"
+hemanth.lastname="Korapati"
+hemanth.age=27
+hemanth.weight=80
+//console.log(hemanth.weight,hemanth.age)
+
+
+let hemanth1={ firstname:"hemanth",lastname:"korapati",height:6,weight:70,phone:{ firstphone:"yureka",secondphone:"iphone",sim:{firstsim:"airtel"}}}
+console.log(hemanth1.phone.sim)
+
+
+
+//2 method
+
+let movie= new Object()
+
+movie.title="kantara"
+movie.budget="45cr"
+movie.actor="Rishab shetty"
+movie.releasedate="10-10-2022"
+//console.log(movie)
